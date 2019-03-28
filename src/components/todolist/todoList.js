3ea8519687d5f95b15b8bc701281fababe3a64b6
zekihe <zekihe@163.com>
2019-03-28 20:28:17 +0800

    add components

--- src/components/todolist/todoList.js
+++ src/components/todolist/todoList.js
@@ -1,5 +1,5 @@
 import React, { Component,Fragment } from 'react';
-import TodoItem from './TodoItem';
+import TodoItem from '../../TodoItem';
 
 class TodoList extends Component {
   constructor(props){
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //App组件，大写字母开头 
-import TodoList from './TodoList';
+import TodoList from './components/todolist/todoList.js';
 
 import './style.css';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

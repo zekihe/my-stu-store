86c0ffea6214eedf5ba98e52e3b6bff55143991c
zekihe <zekihe@163.com>
2019-04-09 20:58:18 +0800

    webpack update

--- vue-todoList/webpack.config.js
+++ vue-todoList/webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+//text 非javascript代码打包成一个单独的静态资源文件，有可能做缓存
+//webpack4安装的时候
+//第一步 npm i extract-text-webpack-plugin@next
+//第二步 contenthash 改成 chunkhash 
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -24,30 +29,30 @@ const config = {
         test: /\.jsx$/,
         loader: 'babel-loader',
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test: /\.styl(us)?$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader', // 能够编译生成sourceMap
-            options: {
-              sourceMap: true, // 若stylus-loader已生成source-map，则postcss-loader就不再重新生成
-              config: {
-                path: './postcss.config.js'
-              }
-            }
-          },
-          'stylus-loader'
-        ]
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //   ]
+      // },
+      // {
+      //   test: /\.styl(us)?$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader',
+      //     {
+      //       loader: 'postcss-loader', // 能够编译生成sourceMap
+      //       options: {
+      //         sourceMap: true, // 若stylus-loader已生成source-map，则postcss-loader就不再重新生成
+      //         config: {
+      //           path: './postcss.config.js'
+      //         }
+      //       }
+      //     },
+      //     'stylus-loader'
+      //   ]
+      // },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
         // 将图片转化成base64 代码，直接写在js内容里面，而不用生成新的文件，对于小图片有用，可减少http请求
@@ -80,23 +85,23 @@ console.log("=======当前环境状态=======", isDev)
 console.log("正在编译...")
 
 if(isDev){
-  // config.module.rules.push({
-  //   test: /\.styl(us)?$/,
-  //   use: [
-  //     'style-loader',
-  //     'css-loader',
-  //     {
-  //       loader: 'postcss-loader', // 能够编译生成sourceMap
-  //       options: {
-  //         sourceMap: true, // 若stylus-loader已生成source-map，则postcss-loader就不再重新生成
-  //         config: {
-  //           path: './postcss.config.js'
-  //         }
-  //       }
-  //     },
-  //     'stylus-loader'
-  //   ]
-  // })
+  config.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader', // 能够编译生成sourceMap
+        options: {
+          sourceMap: true, // 若stylus-loader已生成source-map，则postcss-loader就不再重新生成
+          config: {
+            path: './postcss.config.js'
+          }
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     port: 8010,
@@ -113,6 +118,58 @@ if(isDev){
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+}else {
+  config.entry = {
+    app: path.join(__dirname,'src/index.js'),
+    vendor: ['vue']
+  }
+
+  //webpack 4.0版本写法改动
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2, 
+          maxInitialRequests: 5,
+          minSize: 0
+        },
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10,
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: true   //js单独打包到一个文件里，每个模块都生成一个id
+  }
+  //[chunkhash:8]单独打包的时候使用chunkhash，每个模块都有一个新的hash值
+  //[hash:8]整个应用的hash  
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push({
+      test: /\.styl(us)?$/,
+      use: ExtractPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader', // 能够编译生成sourceMap
+            options: {
+              sourceMap: true, // 若stylus-loader已生成source-map，则postcss-loader就不再重新生成
+              config: {
+                path: './postcss.config.js'
+              }
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+  })
+  config.plugins.push(
+    new ExtractPlugin('style.[chunkhash:8].css')
   )
 }
 
